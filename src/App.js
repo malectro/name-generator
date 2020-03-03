3da3c5a338297c1ccf5c834d05f6a5c33e098078
Kyle Warren <qarren@gmail.com>
2020-03-03 13:09:33 -0800

    added 80s film tiltle names

--- src/App.js
+++ src/App.js
@@ -3,9 +3,65 @@ import logo from './logo.svg';
 import './App.css';
 
 const terms = [
-  ['apple', 'pie', 'time'],
-  ['puppy', 'tree'],
-  ['dude', 'land', 'fire'],
+  [
+    'Miami',
+    'Tijuana',
+    'Malibu',
+    'California',
+    'Sky Resort',
+    'Sorority',
+    'Party',
+    'Bacchanal',
+    'Maui',
+    'Ibiza',
+    'Bangkok',
+    'Vegas',
+    'Private School',
+    'Hollywood',
+    'Summer Camp',
+    'Beach',
+    'Dormitory',
+    'Rock Festival',
+  ],
+  [
+    'Switchblade',
+    'Orgy',
+    'Booty',
+    'Underwear',
+    'Meat Grinder',
+    'Handgun',
+    'Skin Flick',
+    'Brassiere',
+    'Dental Drill',
+    'Lambada',
+    'Lipstick',
+    'Thong',
+    'Roller Derby',
+    'Tequila',
+    'Banana Hammock',
+    'Hot Pants',
+    'High Hell',
+  ],
+  [
+    'Nurses',
+    'Flight Attendants',
+    'Librarians',
+    'Policewomen',
+    'Models',
+    'Tutors',
+    'Coeds',
+    'Strippers',
+    'Lesbian Critical Theory Professors',
+    'Mommies',
+    "Buxom Santa's Elves",
+    'Nuns',
+    'Masseuses',
+    'Prison Guards',
+    'Maids',
+    'Cheerleaders',
+    'Cat Bruglars',
+    'Cavewomen',
+  ],
 ];
 
 const urlHash = window.location.hash;
@@ -16,11 +72,12 @@ function App() {
 
   const handleClick = () => {
     const hash = terms.map(set => Math.floor(Math.random() * set.length));
+    console.log('hio', hash);
     setHash(hash);
     window.history.replaceState(
       {},
       '',
-      '#' + btoa(JSON.stringify(hash)).slice(0, -2),
+      '#' + btoa(JSON.stringify(hash)),
     );
   };
 
@@ -31,7 +88,7 @@ function App() {
         <h2>
           {hash &&
             hash
-              .map((part, index) => terms[index][part % terms.length])
+              .map((part, index) => terms[index][part % terms[index].length])
               .join(' ')}
         </h2>
         <button onClick={handleClick}>Generate</button>
