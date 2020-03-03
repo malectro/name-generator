72abfa0c09e68da5eb87cd3bdb02df8663f29d98
Kyle Warren <qarren@gmail.com>
2020-03-03 12:59:01 -0800

    each generation has a unique url

--- src/App.js
+++ src/App.js
@@ -8,21 +8,34 @@ const terms = [
   ['dude', 'land', 'fire'],
 ];
 
+const urlHash = window.location.hash;
+const defaultHash = urlHash && JSON.parse(atob(urlHash.slice(1)));
+
 function App() {
-  const [hash, setHash] = React.useState();
+  const [hash, setHash] = React.useState(defaultHash);
+
+  const handleClick = () => {
+    const hash = terms.map(set => Math.floor(Math.random() * set.length));
+    setHash(hash);
+    window.history.replaceState(
+      {},
+      '',
+      '#' + btoa(JSON.stringify(hash)).slice(0, -2),
+    );
+  };
 
   return (
     <div className="App">
-      { hash ?
-        <>
-          <h2>{hash.map((part, index) => terms[index][part % terms.length]).join(' ')}</h2>
-        </>
-      :
-        <>
-      <h1>80s Exploitation Film Title Generator</h1>
-          <button onClick={() => setHash(terms.map(set => Math.floor(Math.random() * set.length)))}>Generate</button>
-        </>
-      }
+      <>
+        <h1>80s Exploitation Film Title Generator</h1>
+        <h2>
+          {hash &&
+            hash
+              .map((part, index) => terms[index][part % terms.length])
+              .join(' ')}
+        </h2>
+        <button onClick={handleClick}>Generate</button>
+      </>
     </div>
   );
 }
